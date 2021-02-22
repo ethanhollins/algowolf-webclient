0a58b8c489deff93e1ec8b2d7c104c7fa112f883
Ethan Hollins <ethanjohol@gmail.com>
2021-02-22 20:42:01 +1100

    minor fixes

--- src/components/StrategyApp.jsx
+++ src/components/StrategyApp.jsx
@@ -915,7 +915,7 @@ class StrategyApp extends Component
             console.log('connected');
             this.reconnectCharts();
 
-            const { account, strategyInfo } = this.state;
+            let { account, strategyInfo } = this.state;
             if (account.metadata && account.metadata.open_strategies.length > 0)
             {
                 strategyInfo = this.retrieveStrategies(
